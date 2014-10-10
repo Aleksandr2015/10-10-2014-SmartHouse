257d11c95333c9b212e15c7b474649b7c642ce22
Aleksandr2015 <charleksvik@gmail.com>
2014-10-10 09:25:17 +0300

    добавил функции движка

--- index.php
+++ index.php
@@ -1,52 +1,55 @@
 <!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="<?php bloginfo('charset');?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo('description');?>">    
-    <title><?php the_title(); ?> | <?php bloginfo('name');?></title>
-    <link rel="shortcut icon" href="">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap-theme.css">
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset');?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description');?>">    
+        <title><?php the_title(); ?> | <?php bloginfo('name');?></title>
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap-theme.css">
 
-    <!--[if IE]>
-        <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+        <!--[if IE]>
+            <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <?php if ( is_user_logged_in() ) {echo '<style>.navbar.navbar-inverse.navbar-fixed-top {margin-top:32px;}</style>';} ?>
+        <?php wp_head(); ?>  
+    </head>
 
-<body>
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Умный дом</a>
+                </div>
+
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--.nav-collapse -->
             </div>
+        </nav>
 
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div><!--.nav-collapse -->
-        </div>
-    </nav>
-
-    <div class="container page-home">
-        <div class="row">
-            <div class="col-xs-12">
-                <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/img/1170x900_page-home-bg.png" alt="">
+        <div class="container page-home">
+            <div class="row">
+                <div class="col-xs-12">
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/img/1170x900_page-home-bg.png" alt="">
+                </div>
             </div>
         </div>
-    </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.js"></script>
-</body>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.js"></script>
+        <?php wp_footer(); ?>
+    </body>
 </html>
