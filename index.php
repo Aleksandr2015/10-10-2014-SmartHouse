b0eec34430633e38816d3aafbcd556ab617d34f9
Aleksandr2015 <charleksvik@gmail.com>
2014-10-28 19:11:05 +0200

    Добавил пины согласно присланной картинке

--- index.php
+++ index.php
@@ -27,10 +27,20 @@
             <div class="row">
                 <div class="col-xs-12 ph-bg-cont">
                     <!-- Pins -->
-                    <div class="ph-bg-pin sun-batteries" data-toggle="tooltip" data-placement="left" title="Солнечные батареи">Солнечные батареи</div>
-                    <div class="ph-bg-pin condicioner-1" data-toggle="tooltip" data-placement="left" title="Кондиционер">Кондиционер</div>
-                    <div class="ph-bg-pin condicioner-2" data-toggle="tooltip" data-placement="left" title="Кондиционер">Кондиционер</div>
-                    <div class="ph-bg-pin videocamera" data-toggle="tooltip" data-placement="left" title="Видеокамера">Видеокамера</div>
+                    <a href=""><div class="ph-bg-pin sun-batteries" data-toggle="tooltip" data-placement="left" title="Солнечные батареи">Солнечные батареи</div></a>
+                    <a href=""><div class="ph-bg-pin condicioner-1" data-toggle="tooltip" data-placement="left" title="Кондиционер">Кондиционер</div></a>
+                    <a href=""><div class="ph-bg-pin condicioner-2" data-toggle="tooltip" data-placement="left" title="Кондиционер">Кондиционер</div></a>
+                    <a href=""><div class="ph-bg-pin videocamera" data-toggle="tooltip" data-placement="left" title="Видеокамера">Видеокамера</div></a>
+                    <a href=""><div class="ph-bg-pin weather-stations" data-toggle="tooltip" data-placement="right" title="Погодные станции">Погодные станции</div></a>
+                    <a href=""><div class="ph-bg-pin condicioners-and-ventilations" data-toggle="tooltip" data-placement="left" title="Кондиционеры и вентиляция">Кондиционеры и вентиляция</div></a>
+                    <a href=""><div class="ph-bg-pin fluger" data-toggle="tooltip" data-placement="left" title="Флюгер">Флюгер</div></a>
+                    <a href=""><div class="ph-bg-pin electric-rollets-and-shutters" data-toggle="tooltip" data-placement="left" title="Электро роллеты и ставни">Электро роллеты и ставни</div></a>
+                    <a href=""><div class="ph-bg-pin multimedia" data-toggle="tooltip" data-placement="left" title="Мультимедия">Мультимедия</div></a>
+                    <a href=""><div class="ph-bg-pin fountains-and-ponds" data-toggle="tooltip" data-placement="left" title="Фонтаны и водоемы">Фонтаны и водоемы</div></a>
+                    <a href=""><div class="ph-bg-pin ponds-and-controls" data-toggle="tooltip" data-placement="left" title="Пруды и управление">Пруды и управление</div></a>
+                    <a href=""><div class="ph-bg-pin watering-and-irrigation-controls" data-toggle="tooltip" data-placement="left" title="Управление поливом и орошением">Управление поливом и орошением</div></a>
+                    <a href=""><div class="ph-bg-pin canalization" data-toggle="tooltip" data-placement="left" title="Канализация">Канализация</div></a>
+                    <a href=""><div class="ph-bg-pin water-treatment-system" data-toggle="tooltip" data-placement="left" title="Система водоподготовки">Система водоподготовки</div></a>
                     <!-- Background image -->
                     <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/img/1170x820_page-home-bg.png" alt="">
                 </div>
