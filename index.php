79401fc9aac1e7e02064622986656a21f439f007
Aleksandr2015 <charleksvik@gmail.com>
2014-10-29 08:16:07 +0200

    Добавил все оставшиеся пины

--- index.php
+++ index.php
@@ -40,7 +40,21 @@
                     <a href=""><div class="ph-bg-pin ponds-and-controls" data-toggle="tooltip" data-placement="left" title="Пруды и управление">Пруды и управление</div></a>
                     <a href=""><div class="ph-bg-pin watering-and-irrigation-controls" data-toggle="tooltip" data-placement="left" title="Управление поливом и орошением">Управление поливом и орошением</div></a>
                     <a href=""><div class="ph-bg-pin canalization" data-toggle="tooltip" data-placement="left" title="Канализация">Канализация</div></a>
-                    <a href=""><div class="ph-bg-pin water-treatment-system" data-toggle="tooltip" data-placement="left" title="Система водоподготовки">Система водоподготовки</div></a>
+                    <a href=""><div class="ph-bg-pin water-treatment-system" data-toggle="tooltip" data-placement="left" title="Система водоподготовки">Система водоподготовки</div></a>                    
+                    <a href=""><div class="ph-bg-pin batteries" data-toggle="tooltip" data-placement="left" title="Аккумуляторы">Аккумуляторы</div></a>
+                    <a href=""><div class="ph-bg-pin batteries-2" data-toggle="tooltip" data-placement="left" title="Аккумуляторы">Аккумуляторы</div></a>
+                    <a href=""><div class="ph-bg-pin heating-boilers" data-toggle="tooltip" data-placement="left" title="Котлы отопления">Котлы отопления</div></a>                    
+                    <a href=""><div class="ph-bg-pin boilers" data-toggle="tooltip" data-placement="left" title="Бойлеры">Бойлеры</div></a>                    
+                    <a href=""><div class="ph-bg-pin basin-controls" data-toggle="tooltip" data-placement="right" title="Управление бассейном">Управление бассейном</div></a>                    
+                    <a href=""><div class="ph-bg-pin electronic-locks" data-toggle="tooltip" data-placement="left" title="Электронные замки">Электронные замки</div></a>                    
+                    <a href=""><div class="ph-bg-pin climate-sensors" data-toggle="tooltip" data-placement="left" title="Датчики климата">Датчики климата</div></a>
+                    <a href=""><div class="ph-bg-pin light-sensors" data-toggle="tooltip" data-placement="left" title="Датчики света. Управление светом">Датчики света. Управление светом</div></a>
+                    <a href=""><div class="ph-bg-pin security-systems" data-toggle="tooltip" data-placement="left" title="Системы охраны">Системы охраны</div></a>                    
+                    <a href=""><div class="ph-bg-pin smart-santekhnics" data-toggle="tooltip" data-placement="left" title="Умная сантехника">Умная сантехника</div></a>
+                    <a href=""><div class="ph-bg-pin control-panel" data-toggle="tooltip" data-placement="left" title="Пульт управления">Пульт управления</div></a>
+                    <a href=""><div class="ph-bg-pin warm-floors" data-toggle="tooltip" data-placement="left" title="Теплые полы">Теплые полы</div></a>
+                    <a href=""><div class="ph-bg-pin warm-floors-1" data-toggle="tooltip" data-placement="left" title="Теплые полы">Теплые полы</div></a>
+                    <a href=""><div class="ph-bg-pin septic" data-toggle="tooltip" data-placement="left" title="Септик">Септик</div></a>
                     <!-- Background image -->
                     <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/img/1170x820_page-home-bg.png" alt="">
                 </div>
